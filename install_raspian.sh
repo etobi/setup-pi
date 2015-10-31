13051e4a71367ec2dc3bfb342b013f21045c13e5
Tobias Liebig <work@etobi.de>
2015-10-31 21:25:52 +0100

    [TASK] check requirements

--- install_raspian.sh
+++ install_raspian.sh
@@ -2,11 +2,21 @@
 
 ################################################################
 
-do_download_image() {
-	if (test ! -d "./tmp"); dthen
+do_check() {
+	type dd >/dev/null 2>&1 || { echo >&2 "I require dd but it's not installed.  Aborting."; exit 1; }
+	type wget >/dev/null 2>&1 || { echo >&2 "I require wget but it's not installed.  Aborting."; exit 1; }
+	type pv >/dev/null 2>&1 || { echo >&2 "I require pv but it's not installed.  Aborting."; exit 1; }
+	type jar >/dev/null 2>&1 || { echo >&2 "I require jar but it's not installed.  Aborting."; exit 1; }
+	type diskutil >/dev/null 2>&1 || { echo >&2 "I require diskutil but it's not installed.  Aborting."; exit 1; }
+	
+	if (test ! -d "./tmp"); then
 		mkdir ./tmp
 	fi
-	
+}
+
+################################################################
+
+do_download_image() {
 	read -n 1 -p "Download latest raspbian? [Y/n] " YESNO
 	echo
 	if (test "$YESNO" = "" -o "$YESNO" = "y"  -o "$YESNO" = "Y"); then
@@ -85,6 +95,12 @@ do_dump() {
 
 
 ################################################################
+
+do_check
+
+echo 
+echo 
+echo 
 
 do_download_image
 
