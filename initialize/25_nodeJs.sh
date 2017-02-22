ffc5198d5308a9b32dfb95173d2a5e64f80fa626
Tobias Liebig <mail@etobi.de>
2017-02-22 08:42:37 +0100

    Install node js using apt

--- initialize/25_nodeJs.sh
+++ initialize/25_nodeJs.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # install nodejs 6.x
 
-NODEVERSION=`wget --no-verbose http://nodejs.org/dist/index.tab -O - | grep "^v6." | head -1 | awk '{print $1}'`
-cd /tmpf
-wget http://nodejs.org/dist/latest-v6.x/node-${NODEVERSION}-linux-armv6l.tar.gz
-tar -xvf node-*.tar.gz
-cd node-*/
-sudo cp -R * /usr/local/
-cd
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+apt-get update
+apt-get install nodejs
