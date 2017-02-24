6ce6c441f8a44e3f0d055114d91991f48df60ebe
Tobias Liebig <mail@etobi.de>
2017-02-24 22:41:56 +0100

    disable SendEnv LANG LC_ in  /etc/ssh/ssh_config

--- initialize/01_locale.sh
+++ initialize/01_locale.sh
@@ -6,3 +6,5 @@ sudo bash -c 'sed -i -e "s/# de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/" /etc/locale.g
 sudo bash -c 'echo "LANG=\"de_DE.UTF-8\"" > /etc/default/locale'
 sudo bash -c 'dpkg-reconfigure --frontend=noninteractive locales'
 sudo bash -c 'update-locale LANG=de_DE.UTF-8'
+
+sudo bash -c 'sed -i -e "s/SendEnv LANG LC_/# SendEnv LANG LC_/"  /etc/ssh/ssh_config'
