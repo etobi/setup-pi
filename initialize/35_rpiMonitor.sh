12bb5c502f97795094435e17e22ed928d669ce79
Tobias Liebig <mail@etobi.de>
2017-09-22 15:11:59 +0200

    Update 35_rpiMonitor.sh

--- initialize/35_rpiMonitor.sh
+++ initialize/35_rpiMonitor.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # install rpi monitor
 
-sudo apt-get -y install librrds-perl libhttp-daemon-perl libjson-perl libipc-sharelite-perl libhttp-date-perl libhttp-message-perl liblwp-mediatypes-perl librrd4 libencode-locale-perl libhtml-parser-perl liburi-perl libdbi1 libhtml-tagset-perl libfile-which-perl
-wget https://github.com/XavierBerger/RPi-Monitor-deb/raw/master/packages/rpimonitor_2.12-r0_all.deb -O rpimonitor.deb
-sudo dpkg -i rpimonitor.deb
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F
+sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
 sudo apt-get update
-sudo /usr/share/rpimonitor/scripts/updatePackagesStatus.pl
+sudo apt-get install rpimonitor
+
